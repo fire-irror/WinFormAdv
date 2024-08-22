78b46a6c7ab48d2ccacadce8f061cb571b968c37
fire-irror <firenoah.m@gmail.com>
2024-08-22 11:18:57 +0900

    윈폼 메뉴 작성

--- WinFormAdv/Form1.cs
+++ WinFormAdv/Form1.cs
@@ -16,5 +16,15 @@ namespace WinFormAdv
         {
             InitializeComponent();
         }
+
+        private void 서시ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 줄바꿈ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
